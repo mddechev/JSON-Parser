a2a5f00fff7f8af30e5bc7a8d24046eb7403355e
Mihail Dechev <mihaildechev.work@yahoo.com>
2024-08-14 22:21:14 +0300

    Deleted value pointers in test cases

--- tests/testFactory.cpp
+++ tests/testFactory.cpp
@@ -4,91 +4,112 @@
 
 void testNumberCreation(const String& JSONtext) {
     InputStringStream iss(JSONtext);
+    JSONValue* value = nullptr;
     try {
-        JSONValue* value = JSONFactory::getFactory().createValue(iss);
+        value = JSONFactory::getFactory().createValue(iss);
         value->print();
         std::cout << '\n';
 
         std::cout << "Successfull number creation" << "\n\n";
+        delete value;
     } catch (const CreationError& e) {
+        delete value;
         std::cerr << e.what() << '\n';
     }
 }
 
 void testBoolCreation(const String& JSONtext) {
     InputStringStream iss(JSONtext);
+   JSONValue* value = nullptr;
     try {
-        JSONValue* value = JSONFactory::getFactory().createValue(iss);
+        value = JSONFactory::getFactory().createValue(iss);
         value->print();
         std::cout << '\n';
 
         std::cout << "Successfull bool creation" << "\n\n";
+        delete value;
     } catch (const CreationError& e) {
+        delete value;
         std::cerr << e.what() << '\n';
     }
 }
 
 void testStringCreation(const String& JSONtext) {
     InputStringStream iss(JSONtext);
+    JSONValue* value = nullptr;
     try {
-        JSONValue* value = JSONFactory::getFactory().createValue(iss);
+        value = JSONFactory::getFactory().createValue(iss);
         value->print();
         std::cout << '\n';
 
         std::cout << "Successfull string creation" << "\n\n";
+        delete value;
     } catch (const CreationError& e) {
+        delete value;
         std::cerr << e.what() << '\n';
     }
 }
 
 void testNullCreation(const String& JSONtext) {
     InputStringStream iss(JSONtext);
+    JSONValue* value = nullptr;
     try {
-        JSONValue* value = JSONFactory::getFactory().createValue(iss);
+        value = JSONFactory::getFactory().createValue(iss);
         value->print();
         std::cout << '\n';
 
         std::cout << "Successfull null creation" << "\n\n";
+        delete value;
     } catch (const CreationError& e) {
+        delete value;
         std::cerr << e.what() << '\n';
     }
 }
 
 void testObjectCreation(const String& JSONtext) {
     InputStringStream iss(JSONtext);
+    JSONValue* value = nullptr;
     try {
-        JSONValue* value = JSONFactory::getFactory().createValue(iss);
+        value = JSONFactory::getFactory().createValue(iss);
         value->print();
         std::cout << '\n';
 
         std::cout << "Successfull object creation" << "\n\n";
+        delete value;
     } catch (const CreationError& e) {
+        delete value;
         std::cerr << e.what() << '\n';
     }
 }
 
 void testArrayCreation(const String& JSONtext) {
     InputStringStream iss(JSONtext);
+    JSONValue* value = nullptr;
     try {
-        JSONValue* value = JSONFactory::getFactory().createValue(iss);
+        value = JSONFactory::getFactory().createValue(iss);
         value->print();
         std::cout << '\n';
 
         std::cout << "Successfull array creation" << "\n\n";
+        delete value;
     } catch (const CreationError& e) {
+        delete value;
         std::cerr << e.what() << '\n';
     }
 }
 
 void testValueCreation(const String& JSONtext) {
-     InputStringStream iss(JSONtext);
+    InputStringStream iss(JSONtext);
+    JSONValue* value = nullptr;
     try {
-        JSONValue* value = JSONFactory::getFactory().createValue(iss);
+        value = JSONFactory::getFactory().createValue(iss);
         value->print();
         std::cout << '\n';
 
         std::cout << "Successfull value creation" << "\n\n";
+        delete value;
     } catch (const CreationError& e) {
+        delete value;
         std::cerr << e.what() << '\n';
     }
 }
