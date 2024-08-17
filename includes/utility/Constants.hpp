7491ad2edf0c3c132d90b93faf1c7825ee623a61
Mihail Dechev <mihaildechev.work@yahoo.com>
2024-08-17 17:17:04 +0300

    removed max str len

--- includes/utility/Constants.hpp
+++ includes/utility/Constants.hpp
@@ -10,8 +10,7 @@ const char ARRAY_CLOSING_BRACKET = ']';
 const char OBJECT_OPENING_BRACKET = '{';
 const char OBJECT_CLOSING_BRACKET = '}';
 const char DOUBLE_QUOTE = '\"';
-const size_t MAX_STRING_LENGTH = 1000000;   
-const size_t SUPPORTED_COMMANDS_COUNT = 12;
+
 const char OPEN_COMMAND_NAME[] = "open";
 const char VALIDATE_COMMAND_NAME[] = "validate";
 const char CLOSE_COMMAND_NAME[] = "close";
@@ -26,6 +25,7 @@ const char MOVE_COMMAND_NAME[] = "move";
 const char CONTAINS_COMMAND_NAME[] = "contains";
 const char SEARCH_COMMAND_NAME[] = "search";
 
+const size_t SUPPORTED_COMMANDS_COUNT = 12;
 const size_t NULL_LEN = 4;
 
 #endif //_CONSTANTS_HPP_
