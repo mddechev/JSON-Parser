1a321bd6002bd244aec232d1ec849534c61e0fa4
Mihail Dechev <mihaildechev.work@yahoo.com>
2024-09-08 23:47:16 +0300

    changed method

--- source/json_types/JSONValue.cpp
+++ source/json_types/JSONValue.cpp
@@ -1,6 +1,8 @@
 #include "../../includes/json_types/JSONValue.hpp"
 #include "JSONException.hpp"
 
+//left empty because when exception was throun trough here it broke the search and only
+//object and array override it so not pure virtual
 void JSONValue::search(const String &key, Vector<JSONValue*>& searchResultsArray) const {}
 
 void JSONValue::set(const Vector<String> &path, JSONValue *const value) {
@@ -15,9 +17,10 @@ void JSONValue::remove(const Vector<String> &path) {
     throw InvalidPathError();
 }
 
-bool JSONValue::contains(const String &value) const {
-    return false;
-}
+// bool JSONValue::contains(const String &value) const {
+//     return false;
+// }
+
 
 void JSONValue::printIndent(std::ostream& outputStream, size_t indent) {
     for (size_t i = 0; i < indent; i++) {
