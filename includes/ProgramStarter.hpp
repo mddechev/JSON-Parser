bd7f65ee313b0d3cab73df9dab189fc75a572884
Mihail Dechev <mihaildechev.work@yahoo.com>
2024-09-08 23:50:19 +0300

    changed name from startup

---
+++ includes/ProgramStarter.hpp
@@ -0,0 +1,14 @@
+#ifndef _PROGRAM_STARTER_HPP_
+#define _PROGRAM_STARTER_HPP_
+
+#include "../includes/JSONManager.hpp"
+#include "../includes/commands/CommandHandler.hpp"
+
+//todo change name to ...
+class ProgramStarter {
+public:
+    static void run();
+    static void runDemo();
+};
+
+#endif //_PROGRAM_STARTER_HPP_
